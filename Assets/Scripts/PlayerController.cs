7643abd4813e458c17c960d1adc1b12286582887
christian leppelt <christian.leppelt@gmx.de>
2018-05-10 11:48:37 +1000

    character run animation added, character can only jump once

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,20 +6,42 @@ public class PlayerController : MonoBehaviour {
 
     public Rigidbody2D myRigid;
     public float speed;
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKeyDown(KeyCode.Space))
+    private bool jump = false;
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Space) && jump==false )
         {
             myRigid.velocity = new Vector2(0,9);
+            jump = true;
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Translate(-speed,0,0);
+            Vector2 scale = transform.localScale;
+            if(scale.x > 0)
+                transform.localScale = new Vector2(scale.x * -1, scale.y);
+            if(jump == false)
+            animator.SetBool("running", true);
+            transform.Translate(-speed, 0, 0);
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
+            Vector2 scale = transform.localScale;
+            if (scale.x < 0)
+                transform.localScale = new Vector2(scale.x * -1, scale.y);
+            if (jump == false)
+                animator.SetBool("running", true);
             transform.Translate(speed, 0, 0);
+        }
+        if(Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            animator.SetBool("running", false);
         }
     }
 
@@ -28,6 +50,14 @@ public class PlayerController : MonoBehaviour {
         if (other.gameObject.tag == "Killer")
         {
             this.gameObject.SetActive(false);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Plattform" && jump==true)
+        {
+            jump = false;
         }
     }
 }
