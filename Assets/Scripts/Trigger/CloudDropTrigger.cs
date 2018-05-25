08410482b25fef282db04abc5427b5a5d797dc8a
lance <heyheyheylzx@gmail.com>
2018-05-25 14:24:04 +1000

    scene 4

---
+++ Assets/Scripts/Trigger/CloudDropTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudDropTrigger : MonoBehaviour
+{
+    public GameObject cloud;
+    public GameObject blocker;
+    public float f_speed;
+      void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(blocker);
+            cloud.GetComponent<Rigidbody2D>().velocity = Vector2.down * f_speed;
+        }
+        
+    }
+}
