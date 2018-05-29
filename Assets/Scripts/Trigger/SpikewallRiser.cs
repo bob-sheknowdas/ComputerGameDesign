5880fefcb92bba7419d36f6e0d73aef22963596a
christian leppelt <christian.leppelt@gmx.de>
2018-05-29 22:40:20 +1000

    fixed scene4 a bit

--- Assets/Scripts/Trigger/SpikewallRiser.cs
+++ Assets/Scripts/Trigger/SpikewallRiser.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 public class SpikewallRiser : MonoBehaviour {
 
     public GameObject spikewall;
-    public float speed;
+    public float hight;
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
         {
-            spikewall.GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+            spikewall.GetComponent<Rigidbody2D>().velocity = Vector2.up * 3.2f;
         }
 
 
+    }
+
+    void Update()
+    {
+        if (spikewall.transform.position.y >= hight)
+            spikewall.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
     }
 }
